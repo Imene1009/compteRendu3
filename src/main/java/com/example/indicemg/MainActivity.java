007c836f7d9ec77ca545dd758cca77e2b1e11bbe
dell <khmeissi.imene@gmail.com>
2025-01-11 23:37:58 +0100

    first commit

---
+++ src/main/java/com/example/indicemg/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.indicemg;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Relie cette activité à l'interface activity_main.xml
+    }
+}
